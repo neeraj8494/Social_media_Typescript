c5a62f3d5f6c1fa861df519995d0f2711e322e2c
Neeraj B <neerajb2662001@gmail.com>
2024-03-21 18:47:08 +0530

    initial commit

---
+++ Shopping/src/pages/About.tsx
@@ -0,0 +1,3 @@
+export function About(){
+  return <h1>Your go-to destination for quality and service. We're here to make your experience exceptional. Thank you for choosing us!</h1>
+}
---
+++ Shopping/src/pages/Home.tsx
@@ -0,0 +1,3 @@
+export function Home(){
+  return <h1>In our store section, you'll find all your favorite products!</h1>
+}
